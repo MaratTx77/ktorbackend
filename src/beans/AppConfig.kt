c20a1ceec7378d05cb621ed48bfae33fcd8f02f2
MaratTx77 <maratsr1975@gmail.com>
2020-12-18 20:52:17 +0300

    move setting values to SYSTEM ENVIRONMENT

--- src/beans/AppConfig.kt
+++ src/beans/AppConfig.kt
@@ -1,3 +1,8 @@
+//jdbcUrl = "jdbc:postgresql://localhost:5432/ktor"
+//dbUser = "ktoruser"
+//dbPassword = "ktorUser"
+
+
 package com.example.beans
 
 import com.typesafe.config.ConfigFactory
@@ -9,8 +14,18 @@ object AppConfig {
     private val config = ConfigFactory
         .parseString(this::class.java.classLoader.getResource("settings.conf").readText())
 
-    val dbConfig = config.extract<DbSettings>("db")
-    val jwtConfig = config.extract<JwtSettings>("jwt")
+    val dbConfig : DbSettings = //config.extract<DbSettings>("db")
+        DbSettings(
+            System.getenv("KTOR_JDBC_URL"),
+            System.getenv("KTOR_DB_USER"),
+            System.getenv("KTOR_DB_PASSWORD"))
+    private val jwtConfigShort = config.extract<JwtSettingsShort>("jwt")
+    val jwtConfig = JwtSettings( // hide secret from kotlin source to system environment
+        System.getenv("KTOR_JWT_SECRET"),
+        jwtConfigShort.issuer,
+        jwtConfigShort.accessTokenTimeMin,
+        jwtConfigShort.refreshTokenTimeDays
+    )
     val proxyConfig = config.extract<ProxySettings>("proxy")
 }
 
@@ -24,6 +39,12 @@ data class DbSettings(
     val jdbcUrl: String,
     val dbUser: String,
     val dbPassword: String)
+
+data class JwtSettingsShort(
+    val issuer: String,
+    val accessTokenTimeMin: Int,
+    val refreshTokenTimeDays: Int)
+
 
 data class JwtSettings(
     val secret: String,
